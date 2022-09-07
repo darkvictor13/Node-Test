86c035cf1f08201fbd7c3311b41bd9cbba940bef
darkvictor13 <01victorlmeida2001@hotmail.com>
2022-09-07 20:40:53 -0300

    mudando build e aprendendo route parameters

--- json_hello_world/src/main.ts
+++ json_hello_world/src/main.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const SERVER_PORT = 3000;
 const server = express();
 
-server.get('/', (req, res) => {
+server.get('/', (_req: Request, res: Response) => {
     res.json({ message: 'Hello World!' });
+});
+
+server.get('/ola/:name', (req: Request, res: Response) => {
+    res.json({ message: `Ola ${req.params.name}!` });
 });
 
 server.listen(SERVER_PORT, () => {
